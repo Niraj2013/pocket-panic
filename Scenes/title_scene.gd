74a8f107026993d7085d8542aa5624ed4dd55af9
Niraj2013 <Meenagundecha05644@gmail.com>
2026-08-29 23:24:41 +0530

    Created a Menu/Title Screen

--- Scenes/title_scene.gd
+++ Scenes/title_scene.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+	get_tree().quit()
